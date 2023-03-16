03d51059a44ce7973cda6e9e41b8c3195393b3cd
Hyago Oliveira <hyagogow@gmail.com>
2023-03-16 10:49:31 -0300

    Add TimeDown

---
+++ Assets/Scripts/Levels/TimeDown.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace KitchenChaos.Levels
+{
+    public sealed class TimeDown
+    {
+        public event Action OnStarted;
+        public event Action OnFinished;
+        public event Action<uint> OnUpdated;
+
+        private readonly uint totalTime;
+        private readonly WaitForSecondsRealtime waitOneSecond;
+
+        public TimeDown(uint totalTime)
+        {
+            this.totalTime = totalTime;
+            waitOneSecond = new WaitForSecondsRealtime(1F);
+        }
+
+        internal IEnumerator CountDownRoutine()
+        {
+            var current = totalTime;
+
+            OnStarted?.Invoke();
+
+            do
+            {
+                OnUpdated?.Invoke(current);
+                yield return waitOneSecond;
+                current--;
+            } while (current > 0);
+
+            OnFinished?.Invoke();
+        }
+    }
+}
