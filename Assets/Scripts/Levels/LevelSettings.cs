eb36b678ee8a8d7a30f667a903b91f3d8ebfc857
Hyago Oliveira <hyagogow@gmail.com>
2023-03-16 10:49:48 -0300

    Using TimeDown

--- Assets/Scripts/Levels/LevelSettings.cs
+++ Assets/Scripts/Levels/LevelSettings.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using UnityEngine;
 using ActionCode.AwaitableCoroutines;
 
@@ -8,34 +7,25 @@ namespace KitchenChaos.Levels
     [CreateAssetMenu(fileName = "LevelSettings", menuName = EditorPaths.SO + "Level Settings", order = 110)]
     public sealed class LevelSettings : ScriptableObject
     {
-        [SerializeField, Min(0)] private uint totalCountDown = 3;
+        [SerializeField, Min(0)] private uint initialCountDown = 3;
+        [SerializeField, Min(0)] private uint timeLimit = 120;
 
         public event Action OnLevelStarted;
         public event Action OnLevelFinished;
 
-        public event Action OnCountDownStarted;
-        public event Action<uint> OnCountDownUpdated;
-        public event Action OnCountDownFinished;
+        public TimeDown TimeLimit { get; private set; }
+        public TimeDown InitialCountDown { get; private set; }
 
-        internal void Initialize() => OnLevelStarted?.Invoke();
-
-        internal void StartCountDown() => _ = AwaitableCoroutine.Run(CountDownRoutine());
-
-        private IEnumerator CountDownRoutine()
+        internal void Initialize()
         {
-            var countDown = totalCountDown;
-            var waitOneSecond = new WaitForSecondsRealtime(1F);
+            TimeLimit = new TimeDown(timeLimit);
+            InitialCountDown = new TimeDown(initialCountDown);
 
-            OnCountDownStarted?.Invoke();
-
-            do
-            {
-                OnCountDownUpdated?.Invoke(countDown);
-                yield return waitOneSecond;
-                countDown--;
-            } while (countDown > 0);
-
-            OnCountDownFinished?.Invoke();
+            OnLevelStarted?.Invoke();
         }
+
+        internal void StartTimeLimit() => _ = AwaitableCoroutine.Run(TimeLimit.CountDownRoutine());
+
+        internal void StartInitialCountDown() => _ = AwaitableCoroutine.Run(InitialCountDown.CountDownRoutine());
     }
 }
