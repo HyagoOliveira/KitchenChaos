3ed409635c394dbf9922432580408d67eb00dd98
Hyago Oliveira <hyagogow@gmail.com>
2023-03-17 11:49:37 -0300

    Add IChanceable interface

---
+++ Assets/Scripts/_Globals/Interfaces/IChanceable.cs
@@ -0,0 +1,10 @@
+namespace KitchenChaos
+{
+    /// <summary>
+    /// Interface used on objects able to have a chance to something.
+    /// </summary>
+    public interface IChanceable
+    {
+        public float Chance { get; }
+    }
+}
