cb3db45291a2c505e813b07598a7c2ca3411534a
Hyago Oliveira <hyagogow@gmail.com>
2023-03-20 15:51:57 -0300

    Show Score when count down finishes

--- Assets/Scripts/UI/Score.cs
+++ Assets/Scripts/UI/Score.cs
@@ -1,33 +1,46 @@
 using TMPro;
 using UnityEngine;
 using KitchenChaos.Score;
+using KitchenChaos.Matches;
 
 namespace KitchenChaos.UI
 {
     [DisallowMultipleComponent]
     [RequireComponent(typeof(Animation))]
+    [RequireComponent(typeof(Canvas))]
     public sealed class Score : MonoBehaviour
     {
         [SerializeField] private ScoreSettings settings;
+        [SerializeField] private MatchSettings matchSettings;
         [SerializeField] private Animation animation;
+        [SerializeField] private Canvas canvas;
         [SerializeField] private TMP_Text current;
         [SerializeField] private TMP_Text delta;
         [SerializeField] private Color increasedColor = Color.green;
         [SerializeField] private Color decreasedColor = Color.red;
 
-        private void Reset() => animation = GetComponent<Animation>();
+        private void Reset()
+        {
+            animation = GetComponent<Animation>();
+            canvas = GetComponent<Canvas>();
+        }
+
         private void Awake() => current.text = "0";
 
         private void OnEnable()
         {
             settings.OnScoreIncreased += HandleScoreIncreased;
             settings.OnScoreDecreased += HandleScoreDecreased;
+
+            matchSettings.CountDown.OnFinished += HandleCountDownFinished;
         }
 
         private void OnDisable()
         {
             settings.OnScoreIncreased -= HandleScoreIncreased;
             settings.OnScoreDecreased -= HandleScoreDecreased;
+
+            matchSettings.CountDown.OnFinished -= HandleCountDownFinished;
         }
 
         private void HandleScoreIncreased(float deltaScore)
@@ -41,6 +54,8 @@ namespace KitchenChaos.UI
             delta.color = decreasedColor;
             ShowDelta("- " + deltaScore);
         }
+
+        private void HandleCountDownFinished() => canvas.enabled = true;
 
         private void ShowDelta(string deltaScore)
         {
