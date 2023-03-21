287b698b47215613493407f364c70246f039f61c
Hyago Oliveira <hyagogow@gmail.com>
2023-03-21 16:26:35 -0300

    Add Gradient Slider into Timer

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -1,44 +1,52 @@
 using TMPro;
-using UnityEngine;
-using KitchenChaos.Matches;
 using System;
+using UnityEngine;
+using UnityEngine.UI;
+using KitchenChaos.Matches;
 
 namespace KitchenChaos.UI
 {
     [DisallowMultipleComponent]
+    [RequireComponent(typeof(Canvas))]
     public sealed class Timer : MonoBehaviour
     {
         [SerializeField] private MatchSettings levelSettings;
-        [SerializeField] private GameObject panel;
+        [SerializeField] private Canvas canvas;
         [SerializeField] private TMP_Text timer;
+        [SerializeField] private Slider slider;
         [SerializeField] private string timerFormat = @"mm\:ss";
 
         private void Reset()
         {
+            canvas = GetComponent<Canvas>();
             timer = GetComponentInChildren<TMP_Text>();
+            slider = GetComponentInChildren<Slider>();
         }
 
         private void OnEnable()
         {
-            levelSettings.CountDown.OnStarted += HidePanel;
-            levelSettings.TimeLimit.OnStarted += ShowPanel;
+            levelSettings.CountDown.OnStarted += Hide;
+            levelSettings.TimeLimit.OnStarted += Show;
             levelSettings.TimeLimit.OnUpdated += HandleTimeLimitUpdated;
         }
 
         private void OnDisable()
         {
-            levelSettings.CountDown.OnStarted -= HidePanel;
-            levelSettings.TimeLimit.OnStarted -= ShowPanel;
+            levelSettings.CountDown.OnStarted -= Hide;
+            levelSettings.TimeLimit.OnStarted -= Show;
             levelSettings.TimeLimit.OnUpdated -= HandleTimeLimitUpdated;
         }
 
-        private void HidePanel() => panel.SetActive(false);
-        private void ShowPanel() => panel.SetActive(true);
+        private void Hide() => canvas.enabled = false;
+        private void Show() => canvas.enabled = true;
 
         private void HandleTimeLimitUpdated(uint seconds)
         {
             var time = TimeSpan.FromSeconds(seconds);
             timer.text = time.ToString(timerFormat);
+
+            var normilizedTime = (float)seconds / levelSettings.TimeLimit.totalTime;
+            slider.value = normilizedTime;
         }
     }
 }
