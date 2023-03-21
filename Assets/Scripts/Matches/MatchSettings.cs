b6b52730cbf917f5e3fc8220382aed9d272fb18a
Hyago Oliveira <hyagogow@gmail.com>
2023-03-21 18:08:52 -0300

    Add OnFinalSecondsStarted event

--- Assets/Scripts/Matches/MatchSettings.cs
+++ Assets/Scripts/Matches/MatchSettings.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using System.Collections;
 
 namespace KitchenChaos.Matches
 {
@@ -8,16 +9,16 @@ namespace KitchenChaos.Matches
     {
         [SerializeField, Min(0)] private uint countDown = 3;
         [SerializeField, Min(0)] private uint timeLimit = 120;
+        [SerializeField, Min(0)] private uint finalSeconds = 20;
 
         public event Action OnStarted;
         public event Action OnFinished;
+        public event Action OnFinalSecondsStarted;
 
         public TimeDown TimeLimit { get; private set; } = new TimeDown();
         public TimeDown CountDown { get; private set; } = new TimeDown();
 
         private MatchManager manager;
-        private Coroutine timeLimitRoutine;
-        private Coroutine countDownRoutine;
 
         internal void Initialize(MatchManager manager)
         {
@@ -29,16 +30,23 @@ namespace KitchenChaos.Matches
             OnStarted?.Invoke();
         }
 
-        internal void StartTimeLimit() => timeLimitRoutine = manager.StartCoroutine(TimeLimit.CountDownRoutine());
+        internal void StartTimeLimit()
+        {
+            manager.StartCoroutine(TimeLimit.CountDownRoutine());
+            manager.StartCoroutine(InvokeOnFinalSecondsStartedRoutine());
+        }
 
-        internal void StartCountDown() => countDownRoutine = manager.StartCoroutine(CountDown.CountDownRoutine());
+        internal void StartCountDown() => manager.StartCoroutine(CountDown.CountDownRoutine());
 
         internal void FinishTimeLimit() => OnFinished?.Invoke();
 
-        public void Dispose()
+        private IEnumerator InvokeOnFinalSecondsStartedRoutine()
         {
-            if (timeLimitRoutine != null) manager.StopCoroutine(timeLimitRoutine);
-            if (countDownRoutine != null) manager.StopCoroutine(countDownRoutine);
+            var timeToFinalSeconds = timeLimit - finalSeconds;
+            yield return new WaitForSeconds(timeToFinalSeconds);
+            OnFinalSecondsStarted?.Invoke();
         }
+
+        public void Dispose() => manager.StopAllCoroutines();
     }
 }
