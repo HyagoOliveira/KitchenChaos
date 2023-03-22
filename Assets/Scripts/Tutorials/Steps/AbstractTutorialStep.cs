8bd1991fabc4a650a9405dd7af6b577c4499f4ea
Hyago Oliveira <hyagogow@gmail.com>
2023-03-22 15:45:04 -0300

    Add TutorialManager

---
+++ Assets/Scripts/Tutorials/Steps/AbstractTutorialStep.cs
@@ -0,0 +1,24 @@
+using KitchenChaos.Players;
+using UnityEngine;
+
+namespace KitchenChaos.Tutorials
+{
+    public abstract class AbstractTutorialStep : MonoBehaviour
+    {
+        protected Player CurrentPlayer => manager.PlayerSettings.Current;
+
+        protected TutorialManager manager;
+
+        internal void Initialize(TutorialManager manager) => this.manager = manager;
+
+        private void Start()
+        {
+        }
+
+        internal abstract void Begin();
+
+        protected void Complete() => manager.CompleteStep();
+
+        internal abstract string GetDescription();
+    }
+}
---
+++ Assets/Scripts/Tutorials/TutorialManager.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using System.Collections;
+using KitchenChaos.UI;
+using KitchenChaos.Players;
+
+namespace KitchenChaos.Tutorials
+{
+    [DisallowMultipleComponent]
+    public sealed class TutorialManager : MonoBehaviour
+    {
+        [field: SerializeField] public PlayerSettings PlayerSettings { get; private set; }
+
+        [SerializeField] private GameObject arrow;
+        [SerializeField] private TutorialDescription description;
+        [SerializeField, Min(0f)] private float timeToReadCompleteStep = 1f;
+        [SerializeField, Min(0f)] private float timeBetweenSteps = 2f;
+        [SerializeField] private AbstractTutorialStep[] steps;
+
+        public int TotalSteps => steps.Length;
+        public AbstractTutorialStep CurrentStep => steps[currentIndex];
+
+        private int currentIndex = -1;
+
+        private void Reset()
+        {
+            steps = GetComponents<AbstractTutorialStep>();
+            description = GetComponentInChildren<TutorialDescription>();
+            arrow = transform.Find("TutorialArrow").gameObject;
+        }
+
+        private void Awake()
+        {
+            foreach (var step in steps)
+            {
+                step.Initialize(this);
+            }
+        }
+
+        //TODO apagar
+        private void Start() => GoToNextStep();
+
+        internal void ShowArrow(Vector3 position)
+        {
+            arrow.transform.position = position;
+            arrow.SetActive(true);
+        }
+
+        internal void HideArrow() => arrow.SetActive(false);
+
+        internal void CompleteStep() => StartCoroutine(CompleteStepRoutine());
+
+        private void GoToNextStep() => StartCoroutine(GoToNextStepRoutine());
+
+        private void TryBeginNextStep()
+        {
+            if (++currentIndex >= TotalSteps)
+            {
+                CompleteTutorial();
+                return;
+            }
+
+            description.SetUncompletedText(CurrentStep.GetDescription());
+            CurrentStep.Begin();
+        }
+
+        private void CompleteTutorial()
+        {
+            print("Tutorial is completed");
+            Destroy(gameObject);
+        }
+
+        private IEnumerator CompleteStepRoutine()
+        {
+            description.Complete();
+
+            yield return new WaitForSeconds(timeToReadCompleteStep);
+            yield return description.FadeOutRoutine();
+            yield return GoToNextStepRoutine();
+        }
+
+        private IEnumerator GoToNextStepRoutine()
+        {
+            yield return new WaitForSeconds(timeBetweenSteps);
+            TryBeginNextStep();
+        }
+    }
+}
