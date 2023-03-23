74ce8149e3d9a6a71e340c9f12cb0dc4b94df1dd
Hyago Oliveira <hyagogow@gmail.com>
2023-03-23 16:15:17 -0300

    Add IEnable interface

---
+++ Assets/Scripts/_Globals/Interfaces/IEnable.cs
@@ -0,0 +1,10 @@
+namespace KitchenChaos
+{
+    /// <summary>
+    /// Interface used on objects able to be enabled.
+    /// </summary>
+    public interface IEnable
+    {
+        bool IsEnabled { get; set; }
+    }
+}
