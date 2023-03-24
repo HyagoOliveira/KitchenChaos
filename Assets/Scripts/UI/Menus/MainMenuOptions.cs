9e56ff9edbcba8ff39a1ab55edd4590bcbf37b68
Hyago Oliveira <hyagogow@gmail.com>
2023-03-24 17:13:13 -0300

    Add Tutorial button into MainMenu

--- Assets/Scripts/UI/Menus/MainMenuOptions.cs
+++ Assets/Scripts/UI/Menus/MainMenuOptions.cs
@@ -12,6 +12,7 @@ namespace KitchenChaos.UI
 
         [Header("Buttons")]
         [SerializeField] private DelayedButton playButton;
+        [SerializeField] private DelayedButton tutorialButton;
         [SerializeField] private DelayedButton soundsButton;
         [SerializeField] private DelayedButton creditsButton;
 
@@ -21,6 +22,7 @@ namespace KitchenChaos.UI
         protected override void BindButtonsEvents()
         {
             playButton.onClick.AddListener(PlayGame);
+            tutorialButton.onClick.AddListener(PlayTutorial);
             soundsButton.onClick.AddListener(RequestOpenSoundOptions);
             creditsButton.onClick.AddListener(RequestOpenCredits);
         }
@@ -28,11 +30,13 @@ namespace KitchenChaos.UI
         protected override void UnBindButtonsEvents()
         {
             playButton.onClick.RemoveListener(PlayGame);
+            tutorialButton.onClick.RemoveListener(PlayTutorial);
             soundsButton.onClick.RemoveListener(RequestOpenSoundOptions);
             creditsButton.onClick.RemoveListener(RequestOpenCredits);
         }
 
         private void PlayGame() => sceneSettings.GoToGame();
+        private void PlayTutorial() => sceneSettings.GoToTutorial();
         private void RequestOpenCredits() => OnOpenCreditsRequested?.Invoke();
         private void RequestOpenSoundOptions() => OnOpenSoundsOptionsRequested?.Invoke();
     }
