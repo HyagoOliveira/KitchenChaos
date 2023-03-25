65ba016f3951c7cc5d4f5fabeb18ebbe4a6d94dd
Hyago Oliveira <hyagogow@gmail.com>
2023-03-25 09:33:13 -0300

    Add HighScore property

--- Assets/Scripts/Serialization/GameData.cs
+++ Assets/Scripts/Serialization/GameData.cs
@@ -7,20 +7,23 @@ namespace KitchenChaos.Serialization
     public sealed class GameData
     {
         public string Language { get; internal set; }
+        public uint HighScore { get; internal set; }
         public AudioData Audio { get; internal set; }
         public DateTime CreationTime { get; internal set; }
         public DateTime LastUpdateTime { get; internal set; }
 
-        public GameData() : this(string.Empty, new AudioData(), DateTime.Now, default) { }
+        public GameData() : this(string.Empty, 0, new AudioData(), DateTime.Now, default) { }
 
         public GameData(
             string language,
+            uint highScore,
             AudioData audio,
             DateTime creationTime,
             DateTime lastUpdateTime
         )
         {
             Language = language;
+            HighScore = highScore;
             Audio = audio;
             CreationTime = creationTime;
             LastUpdateTime = lastUpdateTime;
